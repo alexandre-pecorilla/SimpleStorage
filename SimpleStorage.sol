3f14650c21e9d73ca6cf9c1579ab87d41f7ef616
alexandre-pecorilla <alexandre@pecorilla.us>
2026-03-01 16:56:04 -0500

    Initial Commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.18; // stating the version of solidity we will use
+
+contract SimpleStorage { // Create the smart contract and name it - Similar to declaring a class in object oriented programming
+    
+} 
