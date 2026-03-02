d126cc0c68e161b0e2fa210646e3e06b78c261e0
alexandre-pecorilla <alexandre@pecorilla.us>
2026-03-02 15:09:46 -0500

    Arrays and Structs

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,10 +2,27 @@
 pragma solidity 0.8.18; // stating the version of solidity we will use
 
 contract SimpleStorage { // Create the smart contract and name it - Similar to declaring a class in object oriented programming
-    uint256 public favoriteNumber; // If public isn't specified, it defaults to internal
+    uint256 myFavoriteNumber; // If public isn't specified, it defaults to internal
 
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+    //uint256[] listOfFavoriteNumbers;
+
+    // creating a struct
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }    
+
+    // initialize the struct - 2 ways: 
+    // Person public myFriend = Person(11, "Alex");
+    // Person public alex = Person({favoriteNumber: 11, name: "Alex"});
+
+    // creating a dynamic array of persons
+    Person[] public listOfPeople;
+    // a static array => Person[3] public listOfPeople;
+
+
+    function store(uint256 _myFavoriteNumber) public {
+        myFavoriteNumber = _myFavoriteNumber;
     }
 
     // manual getter function for favoriteNumber
@@ -14,6 +31,10 @@ contract SimpleStorage { // Create the smart contract and name it - Similar to d
     // view keywords indicates our function will just read the state of the blockchain, not change it
     // that means calling this function doesn't require a transaction, but just a call to read the state
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
     }
 } 
