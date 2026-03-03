21df1a204742fd7a0c926b548437e882362b25cf
alexandre-pecorilla <alexandre@pecorilla.us>
2026-03-03 14:33:58 -0500

    Interacting with Smart Contracts ABI

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -5,9 +5,26 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
 
-    SimpleStorage public simpleStorage; // SimpleStorage refers to the contract we importer, simpleStorage refers to the variable
-    // function that will deploy a simple storage contract
+    SimpleStorage[] public listOfSimpleStorageContracts; // SimpleStorage refers to the contract we importer, simpleStorage refers to the variable
+    
+    // function that will deploy a SimpleStorage contract
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(new SimpleStorage());
+    }
+
+    // function to store a favorite number on a specified SimpleStorage contract
+    // It calls the store function of the specified SimpleStorage contract
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageFavoriteNumber) public {
+
+        // Get the SimpleStorage contract corresponding to the index
+        SimpleStorage selectedSimpleStorageContract = listOfSimpleStorageContracts[_simpleStorageIndex];
+        // Call the store function on the SimpleStorage contract to assign a new favorite number
+        selectedSimpleStorageContract.store(_newSimpleStorageFavoriteNumber);
+    }
+
+    // Function to fetch the favorite number of a specified SimpleStorage contract
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        SimpleStorage selectedSimpleStorageContract = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return selectedSimpleStorageContract.retrieve();
     }
 }
